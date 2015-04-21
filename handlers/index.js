b34c022814c9f6b9f4fb4067cf337df7d980efe0
phouse512 <philiphouse2015@u.northwestern.edu>
2015-04-21 12:32:17 -0500

    testing

--- handlers/index.js
+++ handlers/index.js
@@ -3,27 +3,28 @@ var parser = require('../util/parser.js'),
 
 
 exports.update = function(req, res, next){
-	datapoints = req.body.split(";");
-	temp = [];
-	for(var i=0; i < datapoints.length; i++){
-		temp.push(parser.parseData(datapoints[i]));
-	}
+	console.log(req.body);
+	// datapoints = req.body.split(";");
+	// temp = [];
+	// for(var i=0; i < datapoints.length; i++){
+	// 	temp.push(parser.parseData(datapoints[i]));
+	// }
 
-	console.log(temp);
+	// console.log(temp);
 
-	DataPoint.create(temp, function (err) {
-    	if (err) { 
-    		console.log(err);
-    		res.send("Invalid data - not stored :(");
-    	}
+	// DataPoint.create(temp, function (err) {
+ //    	if (err) { 
+ //    		console.log(err);
+ //    		res.send("Invalid data - not stored :(");
+ //    	}
 
-    	for (var i=1; i<arguments.length; ++i) {
-    		console.log(arguments[i]);
-        	// var candy = arguments[i];
-    	}
+ //    	for (var i=1; i<arguments.length; ++i) {
+ //    		console.log(arguments[i]);
+ //        	// var candy = arguments[i];
+ //    	}
 
-    	res.send((arguments.length+1) + " documents successfully stored.");
-	});
+ //    	res.send((arguments.length+1) + " documents successfully stored.");
+	// });
 }
 
 exports.index = function(req, res, next) {
